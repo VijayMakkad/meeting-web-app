30b9ceef90f79ecff2fb66c7875f681625f1d3b4
VijayMakkad <vijaymakkad0104@gmail.com>
2024-10-01 14:25:18 +0530

    Clerk added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +26,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <ClerkProvider
+      appearance={{
+        layout:{
+          logoImageUrl:'/icons/logo.svg',
+          socialButtonsVariant:'iconButton'
+        },
+        variables:{
+          colorText:'#fff',
+          colorPrimary:'#0E78F9',
+          colorBackground:'#1c1f2e',
+          colorInputBackground:'#252a41',
+          colorInputText:'#fff'
+        }
+      }}
+      >
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-dark-2 antialiased`}
       >
         {children}
       </body>
+      </ClerkProvider>
     </html>
   );
 }
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -2,24 +2,36 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import MobileNav from "./MobileNav";
+import { UserButton } from "@clerk/nextjs";
+import { SignedIn } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
     <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
-        <Image src="icons/logo.svg" width={32} height={32} alt="GG meeting" className="max-sm:size-10"/>
-        <p className="text-[26px] font-extrabold text-white max-sm:hidden">GG</p>
+        <Image
+          src="icons/logo.svg"
+          width={32}
+          height={32}
+          alt="GG meeting"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          GG
+        </p>
       </Link>
 
       <div className="flex-between gap-5">
-          {/* clerk user management */}
+        {/* clerk user management */}
+        <SignedIn>
+          <UserButton/>
+        </SignedIn>
+
+        {/* Mobile Nav */}
+        <MobileNav />
       </div>
-
-      {/* Mobile Nav */}
-
-      <MobileNav/>
     </nav>
   );
-}; 
+};
 
 export default Navbar;
