f1b8276abcb70f8b959f986bd8f9db70273a20bf
VijayMakkad <vijaymakkad0104@gmail.com>
2024-10-11 23:46:25 +0530

    final

--- components/ui/MeetingModal.tsx
+++ components/ui/MeetingModal.tsx
@@ -2,10 +2,6 @@ import React, { ReactNode } from "react";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
